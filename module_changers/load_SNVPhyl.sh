a9dfc015e46f2c7f68fc189196057775fa34a2ea
nvlachos <6909517+nvlachos@users.noreply.github.com>
2018-10-31 14:01:12 -0400

    uygiby

--- module_changers/load_SNVPhyl.sh
+++ module_changers/load_SNVPhyl.sh
@@ -8,8 +8,9 @@
 
 # Script to load python 3.6.1 (only needed by busco currently)
 # Wouldnt load normally so had to make this file...sorry
+
+module load snvphyl-galaxy-cli/1.3.0
 module unload Python/2.7
 module unload Python/3.5.2
-module load snvphyl-galaxy-cli/1.3.0
-
-
+module unload Python/2.7.13
+module load Python/2.7.15
