f1a6284a9b88bf4f500afba29704b922f1ef7ddf
nvlachos <6909517+nvlachos@users.noreply.github.com>
2018-11-13 09:30:02 -0500

    removed debug printout

--- run_c-sstar_on_single_alternate_DB.sh
+++ run_c-sstar_on_single_alternate_DB.sh
@@ -85,7 +85,7 @@ fi
 alt_database_path=$(basename -- "${5}")
 alt_database=$(echo ${alt_database_path##*/} | cut -d'.' -f1)
 alt_database=${alt_database//_srst2/}
-echo ${alt_database}
+#echo ${alt_database}
 
 # Creates the output c-sstar folder if it does not exist yet
 #echo "${OUTDATADIR}"
