5814ca46a4202367b656325500a0385de42e81f5
nvlachos <6909517+nvlachos@users.noreply.github.com>
2018-11-15 07:28:47 -0500

    checks for config_template

--- outbreak_analysis.sh
+++ outbreak_analysis.sh
@@ -7,6 +7,11 @@
 #$ -q all.q
 
 #Import the config file with shortcuts and settings
+if [[ -f config_template.sh ]]; then
+	if [[ ! -f config.sh ]]; then
+		cp config_template.sh config.sh
+	fi
+fi
 . ./config.sh
 #Import the module file that loads all necessary mods
 . "${mod_changers}/pipeline_mods"
