3a171f88af065fe03ef0c79f04a210f47d436af5
nvlachos <6909517+nvlachos@users.noreply.github.com>
2018-11-16 16:25:27 -0500

    6ujn

--- primary_processing_template_qsub.sh
+++ primary_processing_template_qsub.sh
@@ -630,7 +630,7 @@ check_for_Assemblies() {
 	echo "Entered checker"
 	for sample in "${sample_names[@]}";
 	do
-		#echo "Checking ${sample} at ${main_dir}/${sample}/(plasmid)Assembly/scaffolds.fasta"
+		echo "Checking ${sample} at ${main_dir}/${sample}/(plasmid)Assembly/scaffolds.fasta"
 		if [[ -s "${main_dir}/${sample}/Assembly/scaffolds.fasta" ]]; then
 			# Create script to call normal SPAdes
 			#re qusb Assembly
@@ -646,7 +646,7 @@ check_for_Assemblies() {
 		if [[ -s "${main_dir}/${sample}/plasmidAssembly/scaffolds.fasta" ]]; then
 			# Create script to call plasmid SPAdes
 			#re qsub plasmidAssembly
-			echo "$sample did finished plamsid assembling"
+			echo "$sample finished plamsid assembling"
 			if [[ -f "${main_dir}/${sample}/logs/${sample}_plasmid_assembing_complete.txt" ]]; then
 				echo "Says it finished in logs"
 			else
