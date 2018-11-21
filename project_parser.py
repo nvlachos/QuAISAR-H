b56a2b07006d00f52a5830b6bead3f64807e284d
nvlachos <6909517+nvlachos@users.noreply.github.com>
2018-11-21 14:23:50 -0500

    rthn

--- project_parser.py
+++ project_parser.py
@@ -69,6 +69,8 @@ def do_AR(input_AR, input_plas, output_file):
 			sample_p_plasmids_dict={}
 		source_assembly=line_sections[2]
 		print("Test:"+line_sections[4]+":")
+		if line_sections[4].split("_")[1].split("-")[0] == "contigs":
+			continue
 		plas_perc_id=math.floor(line_sections[4])
 		plas_perc_length=math.floor(100*line_sections[5].split("/")[0]/line_sections[5].split("/")[1])
 		#plas_match_info="["+plas_perc_id+"/"+plas_percpercent_length+"]"
