7776a0a920e55c9fc9d80038f64de4a1c7525a91
nvlachos <6909517+nvlachos@users.noreply.github.com>
2018-12-26 14:52:44 -0500

    fixed mashtree content and extension to be easilyuploaded into microreact

--- outbreak_analysis.sh
+++ outbreak_analysis.sh
@@ -161,7 +161,8 @@ done < ${1}
 # Creating mashtree of all isolates in list
 echo "Creating mashtree of all samples"
 ${shareScript}/mashtree_of_list.sh "${1}" "${output_directory}/mashtree" "${4}"
-cp "${output_directory}/mashtree/${4}.dnd" "${output_directory}"
+cp "${output_directory}/mashtree/${4}.dnd" "${output_directory}/${4}.nwk"
+sed -i "s/_scaffolds_trimmed//g" ${thing}
 rm -r ${output_directory}/mashtree
 
 # Submits the list of isolates that need the newest ResGANNOT file for csstar
