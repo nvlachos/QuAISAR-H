27fe1531131ce2f66bbd67e6c93cd60b350544a6
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-01-09 15:39:43 -0500

    eg

--- determine_taxID.sh
+++ determine_taxID.sh
@@ -69,10 +69,10 @@ Check_source() {
 			largest_line=$(tail -n1 "${processed}/${project}/${sample}/16s/${sample}_16s_blast_id.txt")
 			best_species=$(echo ${best_line} | cut -d'	' -f3)
 			largest_species=$(echo ${largest_line} | cut -d'	' -f3)
-			if [[ ! -z "${largest_species}" ]]; then
+			if [[ ! -z "${largest_species}" ]]  && [[ "${largest_species}" != "" ]]; then
 				do_16s "largest"
 				return
-			elif [[ ! -z "${best_species}" ]]; then
+			elif [[ ! -z "${best_species}" ]]  && [[ "${best_species}" != "" ]]; then
 				do_16s "best"
 				return
 			fi
