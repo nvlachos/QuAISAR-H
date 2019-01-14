b0b2efa1312ef26778ce3df8d5cd47739d8ae556
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-01-14 14:56:11 -0500

    yuk

--- outbreak_analysis.sh
+++ outbreak_analysis.sh
@@ -184,6 +184,7 @@ while IFS= read -r line; do
 	sample_index=0
 	oar_list=""
 	# Looks at all the genes found for a sample
+	ls ${OUTDATADIR}/c-sstar/
 	echo "looking for ${OUTDATADIR}/c-sstar/${sample_name}.${resGANNOT_srst2_filename}.${2}_${3}_sstar_summary.txt"
 	if [[ -f "${OUTDATADIR}/c-sstar/${sample_name}.${resGANNOT_srst2_filename}.${2}_${3}_sstar_summary.txt" ]]; then
 		ARDB_full="${OUTDATADIR}/c-sstar/${sample_name}.${resGANNOT_srst2_filename}.${2}_${3}_sstar_summary.txt"
