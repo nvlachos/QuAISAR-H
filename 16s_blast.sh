ce907ac9943551211f17eab4b3bc1204e6fe41fb
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-02-01 11:24:18 -0500

    seth

--- 16s_blast.sh
+++ 16s_blast.sh
@@ -107,7 +107,7 @@ do
 #		echo "ribo-$ribosome"
 		if [ "${ribosome}" = "16S" ]; then
 			make_fasta $1 $2 $contig $cstart $cstop
-			if [[ "${found_16s}" = "false"]]; then
+			if [[ "${found_16s}" = "false" ]]; then
 				found_16s="true"
 				> ${processed}/${2}/${1}/16s/${1}_16s_rna_seqs.txt
 			fi
