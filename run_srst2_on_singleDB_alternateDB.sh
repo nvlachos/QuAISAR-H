88cfec84f1a5d07ca69206fbc8f8329ba140b5c7
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-03-21 10:41:24 -0400

    removed extra DB name in filename

--- run_srst2_on_singleDB_alternateDB.sh
+++ run_srst2_on_singleDB_alternateDB.sh
@@ -69,7 +69,7 @@ fi
 echo "--input_pe ${processed}/${2}/${1}/trimmed/${1}_S1_L001_R1_001.fastq.gz ${processed}/${2}/${1}/trimmed/${1}_S1_L001_R2_001.fastq.gz --output ${processed}/${2}/${1}/srst2/${alt_DB} --gene_db ${alt_DB_path}"
 
 #python2 "${shareScript}/srst2/scripts/srst2.py" --input_pe "${processed}/${2}/${1}/srst2/${1}_S1_L001_R1_001.fastq.gz" "${processed}/${2}/${1}/srst2/${1}_S1_L001_R2_001.fastq.gz" --output "${processed}/${2}/${1}/srst2/${1}_${alt_DB}" --gene_db "${alt_DB_path}"
-#srst2 --input_pe "${processed}/${2}/${1}/srst2/${1}_S1_L001_R1_001.fastq.gz" "${processed}/${2}/${1}/srst2/${1}_S1_L001_R2_001.fastq.gz" --output "${processed}/${2}/${1}/srst2/${1}_${alt_DB}" --gene_db "${alt_DB_path}"
+srst2 --input_pe "${processed}/${2}/${1}/srst2/${1}_S1_L001_R1_001.fastq.gz" "${processed}/${2}/${1}/srst2/${1}_S1_L001_R2_001.fastq.gz" --output "${processed}/${2}/${1}/srst2/${1}_${alt_DB}" --gene_db "${alt_DB_path}"
 
 rm -r "${processed}/${2}/${1}/srst2/${1}_S1_L001_R1_001.fastq.gz"
 rm -r "${processed}/${2}/${1}/srst2/${1}_S1_L001_R2_001.fastq.gz"
@@ -78,11 +78,11 @@ rm -r "${processed}/${2}/${1}/srst2/"*".pileup"
 
 
 find ${processed}/${2}/${1}/srst2 -type f -name "*_${alt_DB}__*" | while read FILE ; do
-	echo "Doing - $FILE"
+	#echo "Doing - $FILE"
   dirname=$(dirname $FILE)
 	filename=$(basename $FILE)
 	filename="${filename/_${alt_DB}__/__}"
-	echo "${filename}"
+	#echo "${filename}"
   mv "${FILE}" "${dirname}/${filename}"
 done
 
