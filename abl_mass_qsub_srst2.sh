c11510ee8699dc4333a0d12365e2f8bb364d9059
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-03-25 12:48:48 -0400

    Fixed incorrect parameter reference for clobbering

--- abl_mass_qsub_srst2.sh
+++ abl_mass_qsub_srst2.sh
@@ -33,17 +33,17 @@ elif [[ ! -f "${1}" ]]; then
 elif [[ ! -f "${3}" ]]; then
 	echo "${3} (alt_db) does not exist...exiting"
 	exit 1
-elif [[ -z "${5}" ]]; then
+elif [[ -z "${4}" ]]; then
 	echo "Clobberness was not input, be sure to add keep or clobber as 4th parameter...exiting"
 	exit 1
 fi
 
 # Check that clobberness is a valid option
-if [[ "${5}" != "keep" ]] && [[ "${5}" != "clobber" ]]; then
+if [[ "${4}" != "keep" ]] && [[ "${4}" != "clobber" ]]; then
 	echo "Clobberness was not input, be sure to add keep or clobber as 5th parameter...exiting"
 	exit 1
 else
-	clobberness="${5}"
+	clobberness="${4}"
 fi
 
 # create an array of all samples in the list
