6390a5d95b366658ef63db57d3491b8028bf715f
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-04-22 10:57:57 -0400

    4xg

--- kraken2_translate.py
+++ kraken2_translate.py
@@ -120,7 +120,7 @@ def organize_mpas(input_kraken, output_mpa):
 			if taxons[i] != "":
 				if "|".join(taxons[0:i]) in mpa_taxon_counts:
 					mpa_taxon_counts[taxons[0:i]]+=1
-				else
+				else:
 					mpa_taxon_counts[taxons[0:i]]=1
 
 
