d0b95aa8f63c1f84ce6edca97dcd5da9ccb2e96b
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-04-23 08:51:05 -0400

    ecth

--- kraken2_translate.py
+++ kraken2_translate.py
@@ -132,7 +132,7 @@ def organize_mpas(input_kraken, output_mpa):
 			print("Taxons is none")
 
 	print("mpa_taxon_counts length:", len(mpa_taxon_counts))
-	for key in mpa_taxon_counts.keys():
+	for key in sorted(mpa_taxon_counts.keys()):
 		print(key, mpa_taxon_counts[key])
 
 
