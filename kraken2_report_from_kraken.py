c94a04fef3d73b3afb0a47a19cd7dae1d9eba89c
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-04-23 11:18:59 -0400

    werhyb

--- kraken2_report_from_kraken.py
+++ kraken2_report_from_kraken.py
@@ -65,7 +65,7 @@ class taxon_Node:
 
 	def find(self, checkName):
 		for child in self.children:
-			print(child.showtaxID(), checkName)
+			print(len(self.children), child.showtaxID(), checkName)
 			if child.showtaxID == checkName:
 				return child
 		return None
