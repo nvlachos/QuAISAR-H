681cfc9c0120c1d2ed80a742347faa66038b693b
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-04-26 09:41:38 -0400

    aev

--- kraken2_report_from_kraken.py
+++ kraken2_report_from_kraken.py
@@ -140,13 +140,16 @@ class taxon_Node:
 
 	def sort_Children(self):
 		if self.getChildCount() > 1:
+			print("Testing rearrangement")
 			for child_index in range(0, len(self.children)):
+				print("Outer:", child_index)
 				biggest_count=-1
 				biggest_Node_index=-1
 				for child_inner_index in range(child_index, len(self.children)):
 					if self.children[child_inner_index].getCount() > biggest_count:
 						biggest_count = self.children[child_inner_index].getCount()
 						biggest_Node_index = child_index
+				print("biggest:", biggest_count, biggest_Node_index)
 				if biggest_Node_index is not child_index:
 					temp_Node = self.children[child_index]
 					self.children[child_index] = self.children[biggest_Node_index]
