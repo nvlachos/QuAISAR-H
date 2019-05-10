9591ce6f77ba39371d9c9575dd90c770edc2a8b2
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-05-10 15:04:40 -0400

    xerg

--- get_subsequence.py
+++ get_subsequence.py
@@ -18,6 +18,8 @@ import os
 
 sequence = ""
 main_record = SeqIO.read(sys.argv[1],"fasta")
-print(":"+str(sys.argv[2])+":"+str(sys.argv[3])+":")
-search_DNA_seq = main_record.description[int(sys.argv[2]):int(sys.argv[3])+1]
+start = int(sys.argv[2])
+end = int(sys.argv[3]) + 1
+print(":"+str(start)+":"+str(end)+":")
+search_DNA_seq = main_record.description[start:end]
 print(search_DNA_seq)
