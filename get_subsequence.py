8f180f6af98e61ad0fd3f8fe46410aceebb046d2
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-05-10 15:07:06 -0400

    srxcth

--- get_subsequence.py
+++ get_subsequence.py
@@ -21,5 +21,7 @@ main_record = SeqIO.read(sys.argv[1],"fasta")
 start = int(sys.argv[2])
 end = int(sys.argv[3]) + 1
 print(":"+str(start)+":"+str(end)+":")
+print(search_DNA_seq.id)
+print(search_DNA_seq.description)
 search_DNA_seq = main_record.description[start:end]
 print(search_DNA_seq)
