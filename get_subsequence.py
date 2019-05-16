a19d4b4fc63c80acc44833d3c388b33b4c7da406
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-05-16 14:08:44 -0400

    trying rev com

--- get_subsequence.py
+++ get_subsequence.py
@@ -23,7 +23,13 @@ end = int(sys.argv[3])
 print(":"+str(start)+":"+str(end)+":")
 print(main_record.id)
 print(main_record.description)
+
 search_DNA_seq = main_record.seq[start:end]
+reverse_record = Seq(search_DNA_seq, generic_dna)
+print(reverse_record.seq)
+reverse_record=reverse_record.reverse_complement()
+print(reverse_record.seq)
+
 if len(sys.argv) > 4:
     if sys.argv[4] == "REVERSE" or sys.argv[4] == "R":
         search_DNA_seq=search_DNA_seq[::-1]
