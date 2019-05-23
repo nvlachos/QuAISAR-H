7cf1517cece9078d591a3c915753e41792d0b96f
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-05-23 11:17:10 -0400

    Trying to make multi-fasta capable

--- get_subsequence.py
+++ get_subsequence.py
@@ -29,18 +29,28 @@ parser.add_argument('-b', '--begin', help='rev-comp begin position', required=Fa
 parser.add_argument('-f', '--finish', help='rev-comp finish position', required=False, type=int, dest='finish', default=0)
 parameters=parser.parse_args()
 
+with open(parameters.input_file, 'r') as myfile:
+    data=myfile.read().replace('\n', ' ')
+print(data.split(' ').count(">"))
+exit()
+
+
+
+for record in SeqIO.parse(parameters.input_file, "fasta"):
+    if record.id.starts_with(parameters)
+    print(record.id)
 
 sequence = ""
-#main_record = SeqIO.read(sys.argv[1],"fasta")
-main_record = SeqIO.read(parameters.input_file, "fasta")
+#match_record = SeqIO.read(sys.argv[1],"fasta")
+match_record = SeqIO.read(parameters.input_file, "fasta")
 #start = int(sys.argv[2])-1
 #end = int(sys.argv[3])
 print(":"+str(parameters.start)+":"+str(parameters.end)+":")
-print(main_record.id)
-print(main_record.description)
+print(match_record.id)
+print(match_record.description)
 
-search_DNA_seq = main_record.seq[parameters.start:parameters.end]
-reverse_record = SeqRecord(Seq(str(search_DNA_seq)), main_record.id, '', '')
+search_DNA_seq = match_record.seq[parameters.start:parameters.end]
+reverse_record = SeqRecord(Seq(str(search_DNA_seq)), match_record.id, '', '')
 #print(reverse_record.seq)
 reverse_record=reverse_record.reverse_complement()
 #print(reverse_record.seq)
