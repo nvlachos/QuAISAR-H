47e2d3431b9478a9dc425b1dcb264c33d51039fc
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-05-30 14:20:47 -0400

    Fixed wrong variable call

--- abl_mass_qsub_csstar.sh
+++ abl_mass_qsub_csstar.sh
@@ -18,7 +18,7 @@ fi
 #. ./module_changers/list_modules.sh
 
 #
-# Usage ./abl_mass_qsub_csstar.sh path_to_list max_concurrent_submissions output_folder_for_scripts clobberness[keep|clobber]
+# Usage ./abl_mass_qsub_csstar.sh path_to_list max_concurrent_submissions output_folder_for_scripts clobberness[keep|clobber] %ID(optional)[80|95|98|99|100]
 #
 
 # Number regex to test max concurrent submission parametr
@@ -30,7 +30,7 @@ if [[ $# -eq 0 ]]; then
 	exit 1
 # Shows a brief uasge/help section if -h option used as first argument
 elif [[ "$1" = "-h" ]]; then
-	echo "Usage is ./abl_mass_qsub_csstar.sh path_to_list_file(single sample ID per line, e.g. B8VHY/1700128 (it must include project id also)) max_concurrent_submissions output_directory_for_scripts clobberness[keep|clobber]"
+	echo "Usage is ./abl_mass_qsub_csstar.sh path_to_list_file(single sample ID per line, e.g. B8VHY/1700128 (it must include project id also)) max_concurrent_submissions output_directory_for_scripts clobberness[keep|clobber] %ID(optional)[80|95|98|99|100]"
 	exit 1
 elif [[ ! -f "${1}" ]]; then
 	echo "${1} (list) does not exist...exiting"
@@ -59,17 +59,17 @@ if [[ "${5}" != 80 ]] && [[ "${5}" != 95 ]] && [[ "${5}" != 98 ]] && [[ "${5}" !
 	echo "Identity is not one of the presets for csstar and therefore will fail, defaulting to 98..."
 	sim="h"
 else
-	if [ "${3}" == 98 ]; then
+	if [ "${5}" == 98 ]; then
 		sim="h"
-	elif [ "${3}" == 80 ]; then
+	elif [ "${5}" == 80 ]; then
 		sim="l"
-	elif [ "${3}" == 99 ]; then
+	elif [ "${5}" == 99 ]; then
 		sim="u"
-	elif [ "${3}" == 95 ]; then
+	elif [ "${5}" == 95 ]; then
 		sim="m"
-	elif [ "${3}" == 100 ]; then
+	elif [ "${5}" == 100 ]; then
 		sim="p"
-	elif [ "${3}" == 40 ]; then
+	elif [ "${5}" == 40 ]; then
 		sim="o"
 fi
 
