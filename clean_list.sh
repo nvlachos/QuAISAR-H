ddf3a6b899499cc7036d273c71b10471b4dad295
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-06-24 14:28:02 -0400

    kuyd

--- clean_list.sh
+++ clean_list.sh
@@ -34,7 +34,7 @@ fi
 cp ${1} ${1}.original
 dos2unix ${1}.original
 #> ${1}
-while IFS='' read -r line; do
+while IFS= read -r line; do
 	echo "O:${line}"
 	line=$(sed 's/*[[:space:]]*//g')
 	echo "N:${line}:"
