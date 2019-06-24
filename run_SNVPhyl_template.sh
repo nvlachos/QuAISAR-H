1b59e50dac75ffc3e1dc255748de3ce7b755598c
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-06-24 12:28:20 -0400

    ygrd

--- run_SNVPhyl_template.sh
+++ run_SNVPhyl_template.sh
@@ -62,6 +62,7 @@ if [[ -d "${OUTDATADIR}/output" ]]; then
 fi
 
 centroid_filename=$(basename ${1}).centroid
+ex -s +'v/\S/d' -cwq ${centroid_filename}
 python ${shareScript}/Mash_centroid.py ${1} ${OUTDATADIR}/${centroid_filename}
 
 counter=0
