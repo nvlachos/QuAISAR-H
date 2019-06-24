56029b37725e0b191cef3538748475c41004c7d4
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-06-24 14:25:40 -0400

    sexcth

--- clean_list.sh
+++ clean_list.sh
@@ -32,7 +32,8 @@ elif [[ "${1}" = "-h" ]]; then
 fi
 
 cp ${1} ${1}.original
-> ${1}
+dos2unix ${1}.original
+#> ${1}
 while IFS='' read -r line; do
 	echo "O:${line}"
 	line=$(sed 's/*[[:space:]]*//g')
