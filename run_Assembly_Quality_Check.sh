8eb6037493eb8c51dee917eaa5a914819cd77259
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-07-22 09:02:37 -0400

    adding plasflow only check

--- run_Assembly_Quality_Check.sh
+++ run_Assembly_Quality_Check.sh
@@ -12,14 +12,13 @@ if [[ ! -f "./config.sh" ]]; then
 fi
 . ./config.sh
 
-module load quast/4.3
-module load Python2/2.7.15
+ml quast/4.3 Python2/2.7.15
 
 #
 # Checks the Assembly quality  using Toms tool and QUAST and comparing the output of both
 # Important stats are # of contigs, assembly length, n%0 and G/C content
 #
-# Usage ./run_Assembly_Quality_Check.sh   sample_name   run_id
+# Usage ./run_Assembly_Quality_Check.sh   sample_name   run_id [-p]
 #
 
 # Checks for proper argumentation
@@ -37,25 +36,33 @@ elif [[ "${1}" = "-h" ]]; then
 elif [ -z "$2" ]; then
 	echo "Empty project id supplied to run_Assembly_Quality_Check.sh, exiting"
 	exit 1
+elif [[ "${3}" == "-p" ]]; then
+	echo "Only running Assembly quality check on plasFlow assembly"
+	do_plasFlow_only="true"
+else
+	do_plasFlow_only="false"
 fi
 
 # Sets the parent output folder as the sample name folder in the processed samples folder in MMB_Data
 OUTDATADIR="${processed}/${2}/${1}"
 
-# Checks for output folder existence and creates creates if not
-if [ ! -d "$OUTDATADIR/Assembly_Stats" ]; then
-	echo "Creating $OUTDATADIR/Assembly_Stats"
-	mkdir -p "$OUTDATADIR/Assembly_Stats"
-fi
+
 echo "Checking Assembly QC with QUAST"
 # Run QUAST
 # Save current directory and move to output directory because it doesnt know how to redirect output
 owd="$(pwd)"
-cd "${OUTDATADIR}/Assembly_Stats"
-# Call QUAST
-python2 "/apps/x86_64/quast/quast-4.3/quast.py" -o "${OUTDATADIR}/Assembly_Stats" "${OUTDATADIR}/Assembly/${1}_scaffolds_trimmed.fasta"
-mv "${OUTDATADIR}/Assembly_Stats/report.txt" "${OUTDATADIR}/Assembly_Stats/${1}_report.txt"
-mv "${OUTDATADIR}/Assembly_Stats/report.tsv" "${OUTDATADIR}/Assembly_Stats/${1}_report.tsv"
+if [[ "${do_plasFlow_only}" != "true" ]]; then
+	# Checks for output folder existence and creates creates if not
+	if [ ! -d "$OUTDATADIR/Assembly_Stats" ]; then
+		echo "Creating $OUTDATADIR/Assembly_Stats"
+		mkdir -p "$OUTDATADIR/Assembly_Stats"
+	fi
+	cd "${OUTDATADIR}/Assembly_Stats"
+	# Call QUAST
+	python2 "/apps/x86_64/quast/quast-4.3/quast.py" -o "${OUTDATADIR}/Assembly_Stats" "${OUTDATADIR}/Assembly/${1}_scaffolds_trimmed.fasta"
+	mv "${OUTDATADIR}/Assembly_Stats/report.txt" "${OUTDATADIR}/Assembly_Stats/${1}_report.txt"
+	mv "${OUTDATADIR}/Assembly_Stats/report.tsv" "${OUTDATADIR}/Assembly_Stats/${1}_report.tsv"
+else
  if [[ -s "${OUTDATADIR}/plasFlow/Unicycler_assemblies/${1}_uni_assembly/${1}_plasmid_assembly_trimmed.fasta" ]]; then
  	if [ ! -d "$OUTDATADIR/Assembly_Stats_plasmid" ]; then
  		echo "Creating $OUTDATADIR/Assembly_Stats_plasFlow"
@@ -64,13 +71,15 @@ mv "${OUTDATADIR}/Assembly_Stats/report.tsv" "${OUTDATADIR}/Assembly_Stats/${1}_
  	python2 "/apps/x86_64/quast/quast-4.3/quast.py" -o "${OUTDATADIR}/Assembly_Stats_plasFlow" "${OUTDATADIR}/plasFlow/Unicycler_assemblies/2018-20-89_uni_assembly/${1}_plasmid_assembly_trimmed.fasta"
  	mv "${OUTDATADIR}/Assembly_Stats_plasFlow/report.txt" "${OUTDATADIR}/Assembly_Stats_plasFlow/${1}_report.txt"
  	mv "${OUTDATADIR}/Assembly_Stats_plasFlow/report.tsv" "${OUTDATADIR}/Assembly_Stats_plasFlow/${1}_report.tsv"
+ else
+	echo "No plasFlow assembly (${OUTDATADIR}/plasFlow/Unicycler_assemblies/${1}_uni_assembly/${1}_plasmid_assembly_trimmed.fasta)"
  fi
+fi
 
 # Return to original directory
 cd "${owd}"
 
-module unload quast/4.3
-module unload Python/2.7.15
+ml -quast/4.3 -Python/2.7.15
 
 #Show that the script seemingly completed successfully
 exit 0
