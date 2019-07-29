05616cfbc7f343b5e03f21bac150f515d0b4d86e
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-07-29 09:54:28 -0400

    zetg

--- run_gottcha.sh
+++ run_gottcha.sh
@@ -22,7 +22,7 @@ fi
 # !Version 1
 #
 
-ml gottcha krona/2.7
+ml perl/5.12.3  gottcha
 
 # Checks for proper argumentation
 if [[ $# -eq 0 ]]; then
@@ -55,42 +55,46 @@ if [ ! -d "$OUTDATADIR/gottcha/gottcha_S" ]; then  #create outdir if absent
 fi
 
 # Needs perl v5.12.3 to function properly
-#. "${shareScript}/module_changers/perl_5221_to_5123.sh"
-
-### Gottcha Taxonomy Classifier ### in species mode
-ml perl/5.22.1 -perl/5.12.3
-
-zipped1="false"
-zipped2="false"
+. "${shareScript}/module_changers/perl_5221_to_5123.sh"
 
 if [[ ! -f "${OUTDATADIR}/trimmed/${1}.paired.fq" ]]; then
-	if [[ ! -f "${OUTDATADIR}/trimmed/${1}_R1_001.paired.fq" ]]; then
-		if [[ -f "${OUTDATADIR}/trimmed/${1}_R1_001.paired.fq.gz" ]]; then
-			zipped1="true"
-			gunzip < "${OUTDATADIR}/trimmed/${1}_R1_001.paired.fq.gz" > "${OUTDATADIR}/trimmed/${1}_R1_001.paired.fq"
+	if [[ -f "${OUTDATADIR}/trimmed/${1}.paired.fq.gz" ]]; then
+		gunzip -c "${OUTDATADIR}/trimmed/${1}.paired.fq.gz" > "${OUTDATADIR}/trimmed/${1}.paired.fq"
+	else
+		if [[ -f "${OUTDATADIR}/trimmed/${1}_R1_001.paired.fq" ]]; then
+			:
+		elif [[ -f "${OUTDATADIR}/trimmed/${1}_R1_001.paired.fq.gz" ]]; then
+			gunzip -c "${OUTDATADIR}/trimmed/${1}_R1_001.paired.fq.gz" > "${OUTDATADIR}/trimmed/${1}_R1_001.paired.fq"
 		else
-			echo "No R1 to use to make ${1}.paired.fq"
+			echo "R1_001.paired.fq does not exist (either zipped or not)"
 			exit
 		fi
-	fi
-	if [[ ! -f "${OUTDATADIR}/trimmed/${1}_R2_001.paired.fq" ]]; then
-		if [[ -f "${OUTDATADIR}/trimmed/${1}_R2_001.paired.fq.gz" ]]; then
-			zipped2="true"
-			gunzip < "${OUTDATADIR}/trimmed/${1}_R2_001.paired.fq.gz" > "${OUTDATADIR}/trimmed/${1}_R2_001.paired.fq"
+		if [[ -f "${OUTDATADIR}/trimmed/${1}_R2_001.paired.fq" ]]; then
+			:
+		elif [[ -f "${OUTDATADIR}/trimmed/${1}_R2_001.paired.fq.gz" ]]; then
+			gunzip -c "${OUTDATADIR}/trimmed/${1}_R2_001.paired.fq.gz" > "${OUTDATADIR}/trimmed/${1}_R2_001.paired.fq"
 		else
-			echo "No R1 to use to make ${1}.paired.fq"
+			echo "R2_001.paired.fq does not exist (either zipped or not)"
 			exit
+		fi
+		cat "${OUTDATADIR}/trimmed/${1}_R1_001.paired.fq" "${OUTDATADIR}/trimmed/${1}_R2_001.paired.fq" > "${OUTDATADIR}/trimmed/${1}.paired.fq"
+		if [[ -s "${OUTDATADIR}/trimmed/${1}_R2_001.paired.fq.gz" ]]; then
+			rm "${OUTDATADIR}/trimmed/${1}_R2_001.paired.fq"
+		fi
+		if [[ -s "${OUTDATADIR}/trimmed/${1}_R1_001.paired.fq.gz" ]]; then
+			rm "${OUTDATADIR}/trimmed/${1}_R1_001.paired.fq.gz"
 		fi
 	fi
 fi
 
-cat "${OUTDATADIR}/trimmed/${1}_R1_001.paired.fq" "${OUTDATADIR}/trimmed/${1}_R2_001.paired.fq" > "${OUTDATADIR}/trimmed/${1}.paired.fq"
 
+### Gottcha Taxonomy Classifier ### in species mode
 gottcha.pl --mode all --outdir "${OUTDATADIR}/gottcha/gottcha_S" --input "${OUTDATADIR}/trimmed/${1}.paired.fq" --database "${gottcha_db}"
 
-ml -perl/5.22.1 perl/5.12.3
+gzip "${OUTDATADIR}/trimmed/${1}.paired.fq"
+
 # Return perl to 5.22.1
-#. "${shareScript}/module_changers/perl_5123_to_5221.sh"
+. "${shareScript}/module_changers/perl_5123_to_5221.sh"
 
 # Create the krona graphs from each of the analyses
 ktImportText "${OUTDATADIR}/gottcha/gottcha_S/${1}_temp/${1}.lineage.tsv" -o "${OUTDATADIR}/gottcha/${1}_species.krona.html"
@@ -98,18 +102,7 @@ ktImportText "${OUTDATADIR}/gottcha/gottcha_S/${1}_temp/${1}.lineage.tsv" -o "${
 #Create a best hit from gottcha1 file
 "${shareScript}/best_hit_from_gottcha1.sh" "${1}" "${2}"
 
-ml -perl/5.12.3 -gottcha -krona/2.7
-
-if [[ -f "${OUTDATADIR}/trimmed/${1}.paired.fq" ]]; then
-	rm "${OUTDATADIR}/trimmed/${1}.paired.fq"
-fi
-if [[ "${zipped1}" == "true" ]]; then
-	rm "${OUTDATADIR}/trimmed/${1}_R1_001.paired.fq"
-fi
-if [[ "${zipped2}" == "true" ]]; then
-	rm "${OUTDATADIR}/trimmed/${1}_R2_001.paired.fq"
-fi
-
+ml -perl/5.12.3 -gottcha
 
 #Script exited gracefully (unless something else inside failed)
 exit 0
