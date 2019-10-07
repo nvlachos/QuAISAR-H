eea2b49535c7a6fd746f08dada5b4705e0242b85
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-10-07 11:58:15 -0400

    ergv

--- xlsx_converter_py3.py
+++ xlsx_converter_py3.py
@@ -141,7 +141,7 @@ def cell_text_clean(text):
   if "\t".encode("utf-8") in s: warning("Clobbering embedded tab")
   if "\n".encode("utf-8") in s: warning("Clobbering embedded newline")
   if "\r".encode("utf-8") in s: warning("Clobbering embedded carriage return")
-  s = s.replace("\t".encode("utf-8")," ").replace("\n".encode("utf-8")," ").replace("\r".encode("utf-8")," ")
+  s = s.replace("\t".encode("utf-8")," ".encode("utf-8")).replace("\n".encode("utf-8")," ".encode("utf-8")).replace("\r".encode("utf-8")," ".encode("utf-8"))
   return s
 
 for row in rows:
