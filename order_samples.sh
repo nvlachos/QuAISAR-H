38a5d284140410a39cac127764d1b5e5a2363165
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-10-21 15:03:57 -0400

    sec

--- order_samples.sh
+++ order_samples.sh
@@ -36,7 +36,7 @@ show_help () {
 
 # Parse through command line options
 options_found=0
-while getopts ":h?n:p:y" option; do
+while getopts "h?npy:" option; do
 	options_found=$(( options_found + 1 ))
 	case "${option}" in
 		\?)
