860fe838f73ce5e3de1e7550cf48f9a8375d7a24
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-11-01 14:29:33 -0400

    lujgf

--- quaisar_failed_assembly_template.sh
+++ quaisar_failed_assembly_template.sh
@@ -65,8 +65,8 @@ start_time=$(date "+%m-%d-%Y_at_%Hh_%Mm_%Ss")
 sample_name="${1}"
 project="${2}"
 OUTDATADIR="${processed}/${project}"
-if [[ ! -z "${4}" ]]; then
-	OUTDATADIR="${4}/${project}"
+if [[ ! -z "${5}" ]]; then
+	OUTDATADIR="${5}/${project}"
 fi
 
 # Remove old run stats as the presence of the file indicates run completion
