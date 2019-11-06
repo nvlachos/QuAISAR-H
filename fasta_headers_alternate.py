4cacd9af60010252044c7481f689da5065109b43
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-11-06 14:59:52 -0500

    ertvh

--- fasta_headers_alternate.py
+++ fasta_headers_alternate.py
@@ -40,7 +40,7 @@ name='_'.join(name[::-1])
 for record in SeqIO.parse(args.input,"fasta"):
     print(record.id)
     print(name)
-    record.id = record.id.split("_cov")[0].replace("NODE",name)
+    #record.id = record.id.split("_cov")[0].replace("NODE",name)
     contig_num = record.id.split(" ")   #[6].split("_")[-1]
     print(*contig_num, sep = "\n")
     seq_length=len(record)
