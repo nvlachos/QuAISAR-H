56d90c36abca5cdae8588419f8e072d9cd85b6db
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-11-07 15:04:17 -0500

    trcb

--- fasta_headers_alternate.py
+++ fasta_headers_alternate.py
@@ -38,8 +38,7 @@ print(name)
 #name=name[3:]
 #print(name)
 #name='-'.join(name[::])
-print("Name=", name)
-exit()
+#print("Name=", name)
 for record in SeqIO.parse(args.input,"fasta"):
     #print(record.id)
     #print(record.name)
