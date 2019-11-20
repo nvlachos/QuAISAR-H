11dd28fb465b2c73dd13d54a8a58e1fcb736954a
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-11-20 12:57:02 -0500

    ervc

--- outbreak_analysis_including_aniDB2.sh
+++ outbreak_analysis_including_aniDB2.sh
@@ -211,10 +211,10 @@ fi
 
 # Creating mashtree of all isolates in list
 echo "Creating mashtree of all samples"
-#${shareScript}/mashtree_of_list.sh -i "${1}" -d "${output_directory}/mashtree" -o "${4}"
-#cp "${output_directory}/mashtree/${4}.dnd" "${output_directory}/${4}.nwk"
-#sed -i "s/_scaffolds_trimmed//g" "${output_directory}/${4}.nwk"
-#rm -r ${output_directory}/mashtree
+# ${shareScript}/mashtree_of_list.sh -i "${1}" -d "${output_directory}/mashtree" -o "${4}"
+# cp "${output_directory}/mashtree/${4}.dnd" "${output_directory}/${4}.nwk"
+# sed -i "s/_scaffolds_trimmed//g" "${output_directory}/${4}.nwk"
+# rm -r ${output_directory}/mashtree
 
 # Submits the list of isolates that need the newest ResGANNCBI file for csstar
 if [[ "${run_csstar}" = "true" ]]; then
