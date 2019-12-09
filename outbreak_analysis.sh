07b5ec6662cd2acd1ca8b27665bced1f8314722c
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-12-09 14:36:02 -0500

    assignd csstar NO AR found

--- outbreak_analysis.sh
+++ outbreak_analysis.sh
@@ -278,6 +278,7 @@ while IFS= read -r line; do
 		if [[ -z "${csstar_list}" ]] || [[ "${csstar_list}" == "" ]]; then
 			echo "EMPTY-${project}	${sample_name}	No AR genes discovered"
 			echo "${project}	${sample_name}	No AR genes discovered" >> ${output_directory}/${4}-csstar_summary.txt
+			csstar_list="No AR genes discovered"
 		else
 			echo "OCCUPADO-${project}	${sample_name}	:${csstar_list}:"
 			echo "${project}	${sample_name}	${csstar_list}" >> ${output_directory}/${4}-csstar_summary.txt
