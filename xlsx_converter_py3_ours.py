b76b9c1db59d4621341f94dcf5988cb6588a3226
nvlachos <6909517+nvlachos@users.noreply.github.com>
2019-12-17 09:58:55 -0500

    4

--- xlsx_converter_py3_ours.py
+++ xlsx_converter_py3_ours.py
@@ -29,7 +29,7 @@ def parseArgs(args=None):
 def do_conversion(excel_filename, sheetname_in, output_name, run_name):
 	seqlog = pd.read_excel(excel_filename, sheetname=shetname_in)
 	print("Total rows: {0}".format(len(seqlog)))
-	matching_rows = seqlog[(seqlog.Output Folder Name)]
+	matching_rows = seqlog[(seqlog.Output_Folder_Name)]
 	print("Mathcing rows: {0}".format(len(matching_rows)))
 	print(list(matching_rows))
 
