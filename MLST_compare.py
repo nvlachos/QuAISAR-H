d8908ecd5216f460e50a5a09acf39e5623db5b3c
Nick <nvx4@cdc.gov>
2019-12-31 13:37:30 -0500

    testing output

--- MLST_compare.py
+++ MLST_compare.py
@@ -156,6 +156,7 @@ def Species_Cluster_Maker(input_list):
     for isos in input_list:
         Name = isos.split('/')[1]
         MLST = glob.glob('/scicomp/groups/OID/NCEZID/DHQP/CEMB/MiSeqAnalysisFiles/' + isos + '/MLST/*.mlst')
+		print(isos, MLST[0])
         Species = MLST_Species(MLST[0])
         if Species == 'Unknown':
             Stats = '/scicomp/groups/OID/NCEZID/DHQP/CEMB/MiSeqAnalysisFiles/' + isos + '/' + Name + '_pipeline_stats.txt'
